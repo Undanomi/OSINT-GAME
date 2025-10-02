9a03e657b77db75d44e94f4ee9f19e20f54bb620
stokob <entwicklung.st@gmail.com>
2025-10-03 05:22:38 +0900

    fix: メールアドレスの変更

--- src/actions/gogglesLogin.ts
+++ src/actions/gogglesLogin.ts
@@ -93,7 +93,7 @@ export async function validatePasswordResetData(data: PasswordResetData): Promis
   }
 
   // 入力値チェック
-  const correctEmail = 'yuki.nitta.1998@goggles.com';
+  const correctEmail = 'y.nitta.1998@goggles.com';
   const correctBirthday = '1998-04-10';
 
   if (data.email === correctEmail && data.birthday === correctBirthday) {
